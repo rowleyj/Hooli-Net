ea304abeb3c759f03e9814b950a5399272ea7d6d
Mohamed Maklad <makladm1999@gmail.com>
2021-10-25 09:34:25 -0400

    Created and migrated 'Test' model

--- HooliNet/api/models.py
+++ HooliNet/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Test(models.Model):
+    a = models.IntegerField()
+    b = models.IntegerField()
+    product = models.IntegerField()
+
+    def __str__(self):
+        return self.product
