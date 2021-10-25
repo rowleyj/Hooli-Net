a11d35d392f90394affe096a269f84ec67d28bf4
Mohamed Maklad <makladm1999@gmail.com>
2021-10-25 18:16:51 -0400

    updated serializer to work with ModelSerializer

--- HooliNet/api/serializers.py
+++ HooliNet/api/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from .models import Test
 
 
-class TestSerializer(serializers.Serializer):
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ['id', 'a', 'b']
+    
+    '''
     a = serializers.IntegerField()
     b = serializers.IntegerField()
     product = serializers.IntegerField()
 
     def create(self, validated_data):
         return Test.objects.create(validated_data)
+    '''
