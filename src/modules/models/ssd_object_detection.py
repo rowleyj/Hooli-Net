9173206eb3406d6f451288ca260af4d2b56272ab
Mohamed Maklad <makladm1999@gmail.com>
2021-11-04 11:23:34 -0400

    updated file paths and added placeholder functions

--- src/modules/models/ssd_object_detection.py
+++ src/modules/models/ssd_object_detection.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 
 #def car_detection(INP_VIDEO_PATH, OUT_VIDEO_PATH):
-PROTOTXT = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/object_detection/MobileNetSSD_deploy.prototxt'
-MODEL = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/object_detection/MobileNetSSD_deploy.caffemodel'
-INP_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/object_detection/cars4.mp4'
-OUT_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/object_detection/cars_detection.avi'
+PROTOTXT = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/MobileNetSSD_deploy.prototxt'
+MODEL = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/MobileNetSSD_deploy.caffemodel'
+INP_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/cars4.mp4'
+OUT_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/cars_detection.avi'
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",  "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
@@ -41,3 +41,9 @@ while True:
 cap.release()
 out.release()
 cv2.destroyAllWindows()
+
+
+# placeholder function
+def car_detection(PROTOTXT, MODEL, INP_VIDEO_PATH, OUT_VIDEO_PATH):
+    #return true if the video was able to be fully processed and the output was stored correctly; return false otherwise
+    pass
