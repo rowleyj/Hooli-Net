6c3fa04d97803bf046f7fc539a20946c2776452b
Mohamed Maklad <makladm1999@gmail.com>
2021-12-28 17:02:03 -0500

    Create local_variables.py
    add local variables as needed, will be added to the .gitignore to avoid overwriting of other person's local variables

---
+++ src/modules/local_variables.py
@@ -0,0 +1,12 @@
+# module to hold all local variables to ensure we can test api on local systems
+
+
+#!/usr/bin/env python3
+
+
+# Local variables and paths - fill with applicable variables
+tesseract_path =    None
+video_inp_path =    None
+video_out_path =    None
+prototext_path =    None
+model_path =        None
