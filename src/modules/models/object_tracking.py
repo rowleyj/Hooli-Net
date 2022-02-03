4583d0e398cabf14c90191387ce4e78378a40bc6
Mohamed Maklad <makladm1999@gmail.com>
2022-02-03 03:17:12 -0500

    Update object_tracking.py
    added debug option and made tracking a function

--- src/modules/models/object_tracking.py
+++ src/modules/models/object_tracking.py
@@ -13,71 +13,98 @@ import time
 #import local_variables
 
 
-tracking_video_inp_path =    'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars4.mp4'
-tracking_video_out_path =    'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars_tracking.mp4'
+def trackObject(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, frame, bb, vehicleID, debug: bool):
+    if debug:
+        tracking_video_inp_path =    'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars4.mp4' #local_variables.tracking_video_inp_path
+        tracking_video_out_path =    'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars_tracking.mp4'
 
-tracker = cv2.TrackerCSRT_create()
+        initBB = None       #init bounding box coordinates
+        fps = None          #init fps throughput estimator
+    
+    else:
+        tracking_video_inp_path =    INP_VIDEO_PATH
+        tracking_video_out_path =    OUT_VIDEO_PATH
 
-initBB = None       #init bounding box coordinates
-fps = None          #init fps throughput estimator
+        initBB = bb
 
-videoStream = cv2.VideoCapture(tracking_video_inp_path) #local_variables.tracking_video_inp_path)
+    trackingData = {
+        "id": vehicleID,
+        "boxes": []
+    }
 
-#loop over frames
-while True:
-    #get frame
-    ret, frame = videoStream.read()
+    tracker = cv2.TrackerCSRT_create()
 
-    #end of video
-    if frame is None:
-        break
+    videoStream = cv2.VideoCapture(tracking_video_inp_path)
 
-    #resize frame
-    #frame = imutils.resize(frame, width=500)
-    (frameHeight, frameWidth) = frame.shape[:2]
+    #loop over frames
+    while True:
+        #get frame
+        ret, frame = videoStream.read()
 
-    #check if initBB is being used (if an object is being tracked)
-    if initBB is not None:
-        #get new bb corrdinates
-        (isSuccessful, box) = tracker.update(frame)
+        #end of video
+        if frame is None:
+            break
 
-        #check if tracker was successful
-        if isSuccessful:
-            (x, y, w, h) = [int(v) for v in box]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        #resize frame
+        #frame = imutils.resize(frame, width=500)
+        (frameHeight, frameWidth) = frame.shape[:2]
+
+        #check if initBB is being used (if an object is being tracked)
+        if initBB is not None:
+            #get new bb corrdinates
+            (isSuccessful, box) = tracker.update(frame)
+
+            #check if tracker was successful
+            if isSuccessful:
+                trackingData["boxes"].append(box)
+                print(box)
+                (x, y, w, h) = [int(v) for v in box]
+                print((x, y, w, h))
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            else:
+                if not debug:
+                    break
+            
+            if debug:
+                #update fps counter
+                fps.update()
+                fps.stop()
+
+                #init info to be displayed on frame
+                info = [
+                    ("Success", "Yes" if isSuccessful else "No"),
+                    ("FPS", f"{fps.fps():.2f}")
+                ]
+
+                for (i, (k, v)) in enumerate(info):
+                    text = f"{k}: {v}"
+                    cv2.putText(frame, text, (10, h - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
         
-        #update fps counter
-        fps.update()
-        fps.stop()
+        if debug:
+            #output the frame
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
 
-        #init info to be displayed on frame
-        info = [
-            ("Success", "Yes" if isSuccessful else "No"),
-            ("FPS", f"{fps.fps():.2f}")
-        ]
+            #select bb if 's' is clicked or quit if 'q' is clicked
+            if key == ord("s"):
+                #select bb of object to be tracked
+                #press ENTER or SPACE after selecting ROI
+                initBB = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+            
+                #start OpenCV tracker using supplied bb
+                tracker.init(frame, initBB)
 
-        for (i, (k, v)) in enumerate(info):
-            text = f"{k}: {v}"
-            cv2.putText(frame, text, (10, h - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    
-    #output the frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+                #start FPS throughput estimator
+                fps = FPS().start()
+            
+            elif key == ord("q"):
+                break
 
-    #select bb if 's' is clicked or quit if 'q' is clicked
-    if key == ord("s"):
-        #select bb of object to be tracked
-        #press ENTER or SPACE after selecting ROI
-        initBB = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
-    
-        #start OpenCV tracker using supplied bb
-        tracker.init(frame, initBB)
+    videoStream.release()
+    cv2.destroyAllWindows()
 
-        #start FPS throughput estimator
-        fps = FPS().start()
-    
-    elif key == ord("q"):
-        break
+    return trackingData
 
-videoStream.release()
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    trackObject(INP_VIDEO_PATH = None, OUT_VIDEO_PATH = None, frame = None, bb = None, debug = True)
