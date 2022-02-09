c73d3a536d0c7b9c6b217e9a4b8f9168f54435af
Mohamed Maklad <makladm1999@gmail.com>
2022-02-09 04:01:31 -0500

    Update ssd_object_detection.py
    added function dedicated to object detection with tracking. Still need to change while loop for it to work properly.

--- src/modules/models/ssd_object_detection.py
+++ src/modules/models/ssd_object_detection.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import sys
+import local_variables
+import object_tracking
 
 def car_detection(INP_VIDEO_PATH, OUT_VIDEO_PATH):
     PROTOTXT = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/MobileNetSSD_deploy.prototxt'
@@ -48,14 +50,71 @@ car_detection(INP_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/
     OUT_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/plate_detection.mp4')
 
 # placeholder function
-def car_detection(PROTOTXT, MODEL, INP_VIDEO_PATH, OUT_VIDEO_PATH):
-    #return true if the video was able to be fully processed and the output was stored correctly; return false otherwise
-    pass
+def carDetectTrack(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, debug: bool):
+    if debug:
+        video_inp_path = 'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars4.mp4'
+        video_out_path = 'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars_detection.mp4'
+        PROTOTXT = 'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/MobileNetSSD_deploy.prototxt'
+        MODEL = 'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/MobileNetSSD_deploy.caffemodel'
+    
+    else:
+        video_inp_path = INP_VIDEO_PATH
+        video_out_path = OUT_VIDEO_PATH
+        PROTOTXT = local_variables.prototext_path
+        MODEL = local_variables.model_path
+
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",  "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+            
+    cap = cv2.VideoCapture(video_inp_path)
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    frameCounter = 0
+    
+    if debug:
+        out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'),
+                            10, (frame_width, frame_height))
+    
+    sys.path.append("..")
+    
+    while True:
+        ret, frame = cap.read()
+        frameCounter += 1
+        if not ret:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        h, w = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 0.0035, (260, 260), 46)
+        net.setInput(blob)
+        detections = net.forward()
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.43:
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                
+                if debug:
+                    label = "{}: {:.2f}%".format(CLASSES[idx],confidence*100)
+                    cv2.rectangle(frame, (startX, startY), (endX, endY),    COLORS[idx], 2)
+                    out.write(frame)
+                    cv2.imshow('frame', frame)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+    if debug:
+        out.release()
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return True
 
 
 if __name__ == "__main__":
-    PROTOTXT = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/MobileNetSSD_deploy.prototxt'
-    MODEL = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/MobileNetSSD_deploy.caffemodel'
     INP_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/cars4.mp4'
     OUT_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/cars_detection.avi'
-    car_detection(PROTOTXT, MODEL, INP_VIDEO_PATH, OUT_VIDEO_PATH)
+    carDetectTrack(INP_VIDEO_PATH, OUT_VIDEO_PATH)
