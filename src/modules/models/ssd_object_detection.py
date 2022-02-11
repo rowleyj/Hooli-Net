988ee4af8df1af2866938894bd0292a67dd426c6
Mohamed Maklad <makladm1999@gmail.com>
2022-02-11 04:18:28 -0500

    Update ssd_object_detection.py
    updated detectTrack function to add debug functionality and to call the tracking function

--- src/modules/models/ssd_object_detection.py
+++ src/modules/models/ssd_object_detection.py
@@ -49,7 +49,8 @@ def car_detection(INP_VIDEO_PATH, OUT_VIDEO_PATH):
 car_detection(INP_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/plate1.MP4',
     OUT_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/plate_detection.mp4')
 
-# placeholder function
+
+# function that incorporates object detection with tracking
 def carDetectTrack(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, debug: bool):
     if debug:
         video_inp_path = 'C:/Users/Mohamed/OneDrive - McMaster University/Documents/School/University/Fall 2021/Elec Eng 4OI6A/Hooli-Net/src/modules/models/cars4.mp4'
@@ -63,7 +64,7 @@ def carDetectTrack(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, debug: bool):
         PROTOTXT = local_variables.prototext_path
         MODEL = local_variables.model_path
 
-    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",  "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
     COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
     net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
@@ -72,6 +73,7 @@ def carDetectTrack(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, debug: bool):
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     frameCounter = 0
+    carCounter = 0
     
     if debug:
         out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'),
@@ -79,31 +81,47 @@ def carDetectTrack(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, debug: bool):
     
     sys.path.append("..")
     
+    carTracking = {}
+
     while True:
         ret, frame = cap.read()
-        frameCounter += 1
+        
         if not ret:
             break
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        
+        if debug and (cv2.waitKey(1) & 0xFF == ord('q')):
             break
+        
         h, w = frame.shape[:2]
         blob = cv2.dnn.blobFromImage(frame, 0.0035, (260, 260), 46)
         net.setInput(blob)
         detections = net.forward()
+
         for i in np.arange(0, detections.shape[2]):
             confidence = detections[0, 0, i, 2]
             if confidence > 0.43:
                 idx = int(detections[0, 0, i, 1])
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
                 
-                if debug:
-                    label = "{}: {:.2f}%".format(CLASSES[idx],confidence*100)
-                    cv2.rectangle(frame, (startX, startY), (endX, endY),    COLORS[idx], 2)
-                    out.write(frame)
-                    cv2.imshow('frame', frame)
-                    y = startY - 15 if startY - 15 > 15 else startY + 15
-                    cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                if CLASSES[idx] == "car":
+                    carTracking[str(carCounter)] = object_tracking.trackObject(video_inp_path, video_out_path, cap.get(cv2.CAP_PROP_POS_FRAMES), box, carCounter, False)
+                    
+                    # mask car
+                    
+                    carCounter += 1
+
+                    if debug:
+                        print("Box:")
+                        print(box)
+                        (startX, startY, endX, endY) = box.astype("int")
+                        label = "{}: {:.2f}%".format(CLASSES[idx],confidence*100)
+                        cv2.rectangle(frame, (startX, startY), (endX, endY),    COLORS[idx], 2)
+                        out.write(frame)
+                        cv2.imshow('frame', frame)
+                        y = startY - 15 if startY - 15 > 15 else startY + 15
+                        cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+        
+        frameCounter += 1
 
     if debug:
         out.release()
@@ -117,4 +135,4 @@ def carDetectTrack(INP_VIDEO_PATH: str, OUT_VIDEO_PATH: str, debug: bool):
 if __name__ == "__main__":
     INP_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/cars4.mp4'
     OUT_VIDEO_PATH = 'C:/Users/tnaguib/Documents/GitHub/Hooli-Net/src/modules/models/cars_detection.avi'
-    carDetectTrack(INP_VIDEO_PATH, OUT_VIDEO_PATH)
+    carDetectTrack(INP_VIDEO_PATH, OUT_VIDEO_PATH, True)
