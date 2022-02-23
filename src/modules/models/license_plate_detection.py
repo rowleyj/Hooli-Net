be925405546248e5f3a9b2b68db51880b41f437f
Mark-Na <47693146+Mark-Na@users.noreply.github.com>
2022-02-23 18:19:15 -0500

    license plate

--- src/modules/models/license_plate_detection.py
+++ src/modules/models/license_plate_detection.py
@@ -5,9 +5,7 @@ import pytesseract
 import local_variables
 
 pytesseract.pytesseract.tesseract_cmd = local_variables.tesseract_path
-
-<<<<<<< Updated upstream
-=======
+"""
 img = cv2.imread("src\modules\models\plates\platevid2.png",cv2.IMREAD_COLOR)
 img = cv2.resize(img,(1200,800))
 
@@ -67,68 +65,9 @@ import pytesseract
 from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\tnaguib\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
->>>>>>> Stashed changes
 # placeholder function; need to add function arguments
 def license_reading(video_inp_path: str, frame: str):
     bb = selectCar(frame)
-<<<<<<< Updated upstream
-
-    #plate dectection
-
-    img = cv2.imread("src\modules\models\plates\platevid2.png",cv2.IMREAD_COLOR)
-    img = cv2.resize(img,(1200,800))
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-    gray = cv2.bilateralFilter(gray, 11,29, 29) 
-    cv2.imshow('car_gray',gray)
-    edged = cv2.Canny(gray, 20, 200,L2gradient = True) 
-    #cv2.imshow('car_edge',edged)
-    contours = cv2.findContours(edged.copy(), cv2.RETR_CCOMP , cv2.CHAIN_APPROX_NONE)
-    contours = imutils.grab_contours(contours)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-    screenCnt = None
-
-    for c in contours:
-        
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.03 * peri, True)
-    
-        if len(approx) == 4:
-            screenCnt = approx
-            break
-    if screenCnt is None:
-        detected = 0
-        print ("No contour detected")
-    else:
-        detected = 1
-
-    if detected == 1:
-        cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
-
-    mask = np.zeros(gray.shape,np.uint8)
-    new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
-    cv2.imshow('contours',new_image)
-    new_image = cv2.bitwise_and(img,img,mask=mask)
-    #cv2.imshow('bitwise',new_image)
-
-    (x, y) = np.where(mask == 255)
-    (topx, topy) = (np.min(x), np.min(y))
-    (bottomx, bottomy) = (np.max(x), np.max(y))
-    Cropped = gray[topx:bottomx+1, topy:bottomy+1]
-    cv2.imshow('cropped',Cropped)
-
-    text = pytesseract.image_to_string(Cropped, config='--psm 11')
-    print("Detected license plate Number is:",text)
-    img = cv2.resize(img,(300,300))
-    Cropped = cv2.resize(Cropped,(400,200))
-    #cv2.imshow('car',img)
-    #cv2.imshow('Cropped',Cropped)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-=======
     y,x,h,w = bb
     img = cv2.imread(frame,cv2.IMREAD_COLOR)
     img = img[x:y,w:h]
@@ -179,7 +118,6 @@ def license_reading(video_inp_path: str, frame: str):
     return
 
 #plate dectection
->>>>>>> Stashed changes
 
 def selectCar(frame:str):
     image = cv2.imread(frame)
