d766dd210379724296d0bef0c13014f98e07e9b0
Mohamed Maklad <makladm1999@gmail.com>
2022-04-02 14:25:45 -0400

    Update local_variables.py
    removed src to get correct directory

--- src/local_variables.py
+++ src/local_variables.py
@@ -12,20 +12,20 @@ import os
 PATH = os.getcwd()
 
 tesseract_path =    'C:/Program Files/Tesseract-OCR/tesseract.exe'
-video_inp_path =    os.path.join(PATH, 'src', 'modules', 'models', 'cars4.mp4')
-video_out_path =    os.path.join(PATH, 'src', 'modules', 'models', 'cars_detection.mp4')
-prototext_path =    os.path.join(PATH, 'src', 'modules', 'models', 'MobileNetSSD_deploy.prototxt')
-model_path =        os.path.join(PATH, 'src', 'modules', 'models', 'MobileNetSSD_deploy.caffemodel')
+video_inp_path =    os.path.join(PATH, 'modules', 'models', 'cars4.mp4')
+video_out_path =    os.path.join(PATH, 'modules', 'models', 'cars_detection.mp4')
+prototext_path =    os.path.join(PATH, 'modules', 'models', 'MobileNetSSD_deploy.prototxt')
+model_path =        os.path.join(PATH, 'modules', 'models', 'MobileNetSSD_deploy.caffemodel')
 
-tracking_video_inp_path =    os.path.join(PATH, 'src', 'modules', 'models', 'cars4.mp4')
-tracking_video_out_path =    os.path.join(PATH, 'src', 'modules', 'models', 'cars_tracking.mp4')
+tracking_video_inp_path =    os.path.join(PATH, 'modules', 'models', 'cars4.mp4')
+tracking_video_out_path =    os.path.join(PATH, 'modules', 'models', 'cars_tracking.mp4')
 
-detect_track_video_inp_path =   os.path.join(PATH, 'src', 'modules', 'models', 'hwTest', 'testvid6.mp4')
-detect_track_video_out_path =   os.path.join(PATH, 'src', 'modules', 'models', 'hwTest', 'cars_detect_track.mp4')
+detect_track_video_inp_path =   os.path.join(PATH, 'modules', 'models', 'hwTest', 'testvid6.mp4')
+detect_track_video_out_path =   os.path.join(PATH, 'modules', 'models', 'hwTest', 'cars_detect_track.mp4')
 
 final_test_vid_path =   os.path.join(PATH, 'public', 'testvid5.mp4')
 
-plate_image_path = os.path.join(PATH, 'src', 'modules', 'models', 'plates', 'platevid2.png')
+plate_image_path = os.path.join(PATH, 'modules', 'models', 'plates', 'platevid2.png')
 
 backend_url =       'http://0.0.0.0:8080'
 
